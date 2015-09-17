ca96e1f74d36c0808c4da1a0de9213a914955581
HongHero <791414379@qq.com>
2015-09-17 21:41:03 +0800

    Initial Commit

---
+++ TableViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewTest
+//
+//  Created by 覃玉红 on 15/9/17.
+//  Copyright (c) 2015年 覃玉红. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewTest
+//
+//  Created by 覃玉红 on 15/9/17.
+//  Copyright (c) 2015年 覃玉红. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
